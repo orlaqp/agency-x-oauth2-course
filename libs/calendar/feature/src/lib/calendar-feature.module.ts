d5ce6b83939f44a90f37daf38204f9ccf09a50a5
Orlando Quero <orlaqp@gmail.com>
2020-07-25 14:15:45 -0400

    added payments and calendar placeholder

--- libs/calendar/feature/src/lib/calendar-feature.module.ts
+++ libs/calendar/feature/src/lib/calendar-feature.module.ts
@@ -3,11 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CalendarComponent } from './containers/calendar/calendar.component';
 import { SharedModule } from '@agency-x/shared/shared';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+
+FullCalendarModule.registerPlugins([
+    dayGridPlugin,
+    timeGridPlugin,
+    listPlugin,
+    interactionPlugin
+  ]);
 
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
+        FullCalendarModule,
 
         RouterModule.forChild([
             /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
