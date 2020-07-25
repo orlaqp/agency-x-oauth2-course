9c0909aa79763e923f812377ce9e6fa79316ddd6
Orlando Quero <orlaqp@gmail.com>
2020-07-25 09:02:04 -0400

    added title to widget component and updated dashboard accordingly

--- libs/shared/shared/src/lib/components/widget/widget.component.ts
+++ libs/shared/shared/src/lib/components/widget/widget.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    ChangeDetectionStrategy,
+    Input,
+} from '@angular/core';
 
 @Component({
-  selector: 'agency-x-widget',
-  templateUrl: './widget.component.html',
-  styleUrls: ['./widget.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'agency-x-widget',
+    templateUrl: './widget.component.html',
+    styleUrls: ['./widget.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WidgetComponent implements OnInit {
+    @Input()
+    title: string;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
